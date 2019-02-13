1e882e9613f85bc15a78d8b5e3a48f661d0da633
meichholz <tim.eichholz98@web.de>
2019-02-13 17:21:31 +0100

    auto

--- config.el
+++ config.el
@@ -2,8 +2,8 @@
 
 (load! "./dash-at-point.el")
 (load! "./winum.el")
-;; (load! "./lispyville.el")
-;; (load! "./packages/python-lsp-ms.el")
+(load! "./lispyville.el")
+(load! "./packages/python-lsp-ms.el")
 (load! "./functions.el")
 (load! "./vars.el")
 (load! "./keybindings.el")
@@ -16,10 +16,10 @@
 (def-package! drag-stuff)
 (def-package! auto-yasnippet)
 (def-package! major-mode-hydra)
-;; (setq make-backup-files t)
-;; (setq lsp-python-ms-dir
-;;       (expand-file-name "~/GitHub/python-language-server/output/bin/Release/"))
-;; (setq lsp-python-ms-executable
-;;       (expand-file-name "~/.local/bin/Microsoft.Python.LanguageServer"))
+(setq make-backup-files t)
+(setq lsp-python-ms-dir
+      (expand-file-name "~/GitHub/python-language-server/output/bin/Release/"))
+(setq lsp-python-ms-executable
+      (expand-file-name "~/.local/bin/Microsoft.Python.LanguageServer"))
 (add-hook! eshell-mode
   (company-mode 1))
